40ecfe9f3e351dcc93dbe7313e9d0d4eb05c2843
Michael Bachmann <mbachmann@bbmsoft.net>
2023-02-04 16:12:15 +0100

    deploy script upgrades helm chart

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,5 @@ git tag v$VERSION &&
     docker build -t babymotte/worterbuch-explorer:latest -t babymotte/worterbuch-explorer:$VERSION . &&
     docker push babymotte/worterbuch-explorer:latest &&
     docker push babymotte/worterbuch-explorer:$VERSION &&
-    git push && git push --tags
+    git push && git push --tags &&
+    helm upgrade worterbuch-explorer chart/
