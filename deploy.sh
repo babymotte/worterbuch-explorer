6fd0b20c4790e5cfe32841301ab3e1d0078d6587
Michael Bachmann <mbachmann@bbmsoft.net>
2025-02-19 18:26:16 +0100

    writing chart as yaml

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ git tag v$VERSION &&
     docker build -t babymotte/worterbuch-explorer:latest -t babymotte/worterbuch-explorer:$VERSION . &&
     docker push babymotte/worterbuch-explorer:latest &&
     docker push babymotte/worterbuch-explorer:$VERSION &&
-    CHART=$(yq <chart/Chart.yaml ".version=\"$VERSION\"|.appVersion=\"$VERSION\"") &&
+    CHART=$(yq -y ".version=\"$VERSION\"|.appVersion=\"$VERSION\"" <chart/Chart.yaml) &&
     echo "$CHART" >chart/Chart.yaml && git add chart/Chart.yaml && git commit -m "updated helm chart" &&
     git push && git push --tags &&
     helm upgrade worterbuch-explorer chart/
