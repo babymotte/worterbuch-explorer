257dd8666325f9391150523d8a653faa6bc0a471
Michael Bachmann <mbachmann@bbmsoft.net>
2025-02-24 11:51:06 +0100

    updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 VERSION=$(jq <package.json -r '.version')
 git tag v$VERSION &&
-    docker build -t babymotte/worterbuch-explorer:latest -t babymotte/worterbuch-explorer:$VERSION . &&
-    docker push babymotte/worterbuch-explorer:latest &&
-    docker push babymotte/worterbuch-explorer:$VERSION &&
+    docker build --push -t babymotte/worterbuch-explorer:latest -t babymotte/worterbuch-explorer:$VERSION . &&
     CHART=$(yq -y ".version=\"$VERSION\"|.appVersion=\"$VERSION\"" <chart/Chart.yaml) &&
     echo "$CHART" >chart/Chart.yaml && git add chart/Chart.yaml && git commit -m "updated helm chart" &&
     git push && git push --tags &&
