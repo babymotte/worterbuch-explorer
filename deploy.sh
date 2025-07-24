9c0d3c95657df23a693a85073f00c32a83f09f3b
Michael Bachmann <mbachmann@bbmsoft.net>
2025-07-24 12:04:05 +0200

    updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ git tag v$VERSION &&
     CHART=$(yq -y ".version=\"$VERSION\"|.appVersion=\"$VERSION\"" <chart/Chart.yaml) &&
     echo "$CHART" >chart/Chart.yaml && git add chart/Chart.yaml && git commit -m "updated helm chart" &&
     git push && git push --tags &&
-    helm upgrade worterbuch-explorer chart/
+    helm upgrade -n worterbuch worterbuch-explorer chart/
