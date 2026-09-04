978f5c6e655a839ae1345ccf8480dc212d97db61
Michael Bachmann <mbachmann@bbmsoft.net>
2026-09-04 15:08:35 +0200

    using annotated tag in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 VERSION=$(jq <package.json -r '.version')
-git tag v$VERSION &&
+git tag v$VERSION -m "$VERSION" &&
     docker build --push -t babymotte/worterbuch-explorer:latest -t babymotte/worterbuch-explorer:$VERSION . &&
     CHART=$(yq -y ".version=\"$VERSION\"|.appVersion=\"$VERSION\"" <chart/Chart.yaml) &&
     echo "$CHART" >chart/Chart.yaml && git add chart/Chart.yaml && git commit -m "updated helm chart" &&
