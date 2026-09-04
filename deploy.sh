d8472135ac50303ab1e9f65f16d196c3a4f3e2fe
Michael Bachmann <mbachmann@bbmsoft.net>
2026-09-04 15:09:29 +0200

    removed chart update from deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,5 @@
 VERSION=$(jq <package.json -r '.version')
 git tag v$VERSION -m "$VERSION" &&
     docker build --push -t babymotte/worterbuch-explorer:latest -t babymotte/worterbuch-explorer:$VERSION . &&
-    CHART=$(yq -y ".version=\"$VERSION\"|.appVersion=\"$VERSION\"" <chart/Chart.yaml) &&
-    echo "$CHART" >chart/Chart.yaml && git add chart/Chart.yaml && git commit -m "updated helm chart" &&
     git push && git push --tags &&
     helm upgrade -n worterbuch worterbuch-explorer chart/
